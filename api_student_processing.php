33e0b086cbba7107a74d599ade6433dff51d99f5
PhamVanThanhG <robertthanh1107@gmail.com>
2022-05-13 21:15:50 +0700

    Thanh

--- api_student_processing.php
+++ api_student_processing.php
@@ -1,60 +1,65 @@
 <?php
-require('Connect.php');
-$query = "";
-$id = "";
-$student_name = "";
-$username = "";
-$pwd = "";
-$img = "";
+require("config.php");
+require("db.php");
+require("SinhVien.php");
+$sv = new SinhVien();
 $method = $_SERVER['REQUEST_METHOD'];
+//Use to get data from request method that is not get, post
+function parseInput()
+{
+    $data = file_get_contents("php://input");
+
+    if ($data == false)
+        return array();
+
+    parse_str($data, $result);
+
+    return $result;
+}
 switch ($method) {
     case 'POST':
-        //Here Handle POST Request
-        echo 'You are using ' . $method . ' Method';
+        echo 'You are using ' . $method . ' Method' . "\n";
+        //Get data from post request method
         $id = isset($_POST['id']) ? $_POST['id'] : "";
         $student_name = isset($_POST['student_name']) ? $_POST['student_name'] : "";
         $username = isset($_POST['username']) ? $_POST['username'] : "";
         $pwd = isset($_POST['pwd']) ? $_POST['pwd'] : "";
         $img = isset($_POST['img']) ? $_POST['img'] : null;
-        echo "$id, $student_name, $username, $pwd ";
-        $query = "INSERT INTO `tbl_student`(`id`, `student_name`, `username`, `pwd`, `img`) VALUES ('$id','$student_name','$username','$pwd', $img)";
-        if (mysqli_query($connect,$query)) {
-            echo "Success";
+        //query
+        $query = "INSERT INTO `tbl_student`(`id`, `student_name`, `username`, `pwd`, `img`) VALUES ('$id','$student_name','$username','$pwd', '$img')";
+        if (mysqli_query($sv::$connection, $query)) {
+            echo "Success to add student with id: " . $id;
         } else {
-            mysqli_error($connect);
             echo "Fail";
         }
         break;
     case 'PUT':
         //Here Handle PUT Request
-        echo 'You are using ' . $method . ' Method';
-        parse_str(file_get_contents('php://input'), $_PUT);
-        var_dump($_PUT);
-        echo $_PUT['id']; 
+        echo 'You are using ' . $method . ' Method' . "\n";
+        //Get data from put request method
+        $_PUT = parseInput();
         $id = isset($_PUT['id']) ? $_PUT['id'] : "";
         $student_name = isset($_PUT['student_name']) ? $_PUT['student_name'] : "";
         $username = isset($_PUT['username']) ? $_PUT['username'] : "";
         $pwd = isset($_PUT['pwd']) ? $_PUT['pwd'] : "";
         $img = isset($_PUT['img']) ? $_PUT['img'] : null;
-        echo "$id, $student_name, $username, $pwd ";
         $query = "UPDATE `tbl_student` SET `student_name`='$student_name',`username`='$username',`pwd`='$pwd',`img`='$img' WHERE `id`='$id'";
-        if (mysqli_query($connect,$query)) {
-            echo "Success";
+        if (mysqli_query($sv::$connection, $query)) {
+            echo "Success to edit student with id: " . $id;
         } else {
-            mysqli_error($connect);
             echo "Fail";
         }
         break;
-   
+
     case 'DELETE':
-        //Here Handle DELETE Request
-        echo 'You are using ' . $method . ' Method';
-        parse_str(file_get_contents('php://input'), $_DELETE);
-        echo $_DELETE['id']; 
+        //Here Handle PUT Request
+        echo 'You are using ' . $method . ' Method' . "\n";
+        //Get data from put request method
+        $_DELETE = parseInput();
         $id = isset($_DELETE['id']) ? $_DELETE['id'] : "";
-         $query = "DELETE FROM `tbl_student` WHERE `id` = '$id'";
-        if (mysqli_query($connect,$query)) {
-            echo "Success";
+        $query = "DELETE FROM `tbl_student` WHERE `id` = '$id'";
+        if (mysqli_query($sv::$connection, $query)) {
+            echo "Success to delete student with id: " . $id;
         } else {
             echo "Fail";
         }
